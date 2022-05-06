2f0af783dcd4554eec3efbe302626d4a00f251e6
Mureed Sultan <mureedsultangeni@gmail.com>
2022-05-06 10:36:13 +0500

    Homepage is done with out responsive

--- src/scripts/app/site/init.js
+++ src/scripts/app/site/init.js
@@ -3,8 +3,11 @@ import {addPage, addPaths} from 'setjs/kernel/page-manager.js';
 import './api.js';
 import './template-functions.js';
 import home from './pages/home.js';
+// import about from './pages/about.js';
 
 export default function() {
   addPage('', home);
-  addPaths('site', []);
+  addPaths('site', [
+    'about'
+  ]);
 }
---
+++ src/scripts/app/site/pages/about.js
@@ -0,0 +1,8 @@
+import getComp from 'setjs/template/component.js';
+
+export default {
+  templates: ['site/about'],
+  comp: function() {
+    return getComp('site/about');
+  }
+};
